2f2df94c7d8ad916e340ced5724139a0fcd7e448
Macintosh Hd <macintoshhd@MacBook-Pro-cua-Macintosh.local>
2020-04-09 15:16:55 +0700

    9-controller

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const userRoute = require('./routes/user.route');
+
+const port = 3000;
+
+const app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.get('/', (req, res) => res.render('index', {
+  name: 'AAA'
+}));
+
+app.use('/users', userRoute);
+
+app.listen(port, () => console.log('Server listening on port', port));
